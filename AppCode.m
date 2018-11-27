d5513b7be1708fe45e63f9dd2edb3c6a7104d0f2
scheung97 <spendell5@yahoo.com>
2018-11-26 23:27:10 -0500

    added case statement for button presses

--- AppCode.m
+++ AppCode.m
@@ -1,2 +1,19 @@
-import Data.x 
+import %signal from android app 
 
+i = input(%imported signal%); 
+
+switch i
+	case l
+		%output signal to move left
+	case u
+		%output signal to move up
+	case d
+		%output signal to go down
+	case r 
+		%output signal to go right
+	otherwise
+		%do nothing
+end
+
+%the output signals should correspond to the input for the VR simulator
+%unsure how to get data from the android app
