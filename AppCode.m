4313941a86d8ce7bb9fe277ced48f93fc0b8b237
scheung97 <spendell5@yahoo.com>
2018-12-05 12:10:41 -0500

    need to find way to break the while loop and input an exit command

--- AppCode.m
+++ AppCode.m
@@ -1,22 +1,27 @@
-prompt = 'Please select direction (F,L,R): '); 
+clear all, close all; 
 
-i = input(prompt,'s');
 
-if isempty(i)
-	i = 'z'; 
-end 
-
-switch i
-	case 'F'||'f'
-		fprintf('U = 30 \n'); 
-	case 'L'||'l'
-		fprintf('U = 28 \n');
-	case 'R'||'r'
-		fprintf('U = 29 \n'); 
-	case 'B'||'b'
-		fprintf('U = 31 \n');
-	otherwise
-		fprintf('Not valid input'); 
+x = 1; 
+while x == 1 
+    prompt = 'Please select direction (F,L,R,B): '; 
+    i = input(prompt,'s');
+    
+    if isempty(i)
+        i = 'z';
+        fprintf('Please enter an input \n\n'); 
+    else
+        switch i
+            case 'F' 
+                fprintf('U = 30 \n'); 
+            case 'L'
+                fprintf('U = 28 \n');
+            case 'R'
+                fprintf('U = 29 \n'); 
+            case 'B'
+                fprintf('U = 31 \n');
+            otherwise
+                fprintf('Not valid input \n' ); 
+        end
+        
+    end
 end
-
-
